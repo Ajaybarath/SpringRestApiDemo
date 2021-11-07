cf851cb4d2279ae58e75829f35d7ba7a9da14351
ajaybarath <ajaybarath3@gmail.com>
2021-11-07 15:27:07 +0530

    Added: controller for get request with params

--- src/main/java/com/bridgelabz/springrestapidemo/controller/HelloRestController.java
+++ src/main/java/com/bridgelabz/springrestapidemo/controller/HelloRestController.java
@@ -1,8 +1,6 @@
 package com.bridgelabz.springrestapidemo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
@@ -11,5 +9,10 @@ public class HelloRestController {
     @GetMapping(value = {"", "/"})
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+    @RequestMapping(value = {"/query"}, method = RequestMethod.GET)
+    public String sayHelloParams(@RequestParam("name") String name) {
+        return "Hello " + name + "!";
     }
 }
